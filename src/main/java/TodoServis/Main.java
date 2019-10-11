8eb470c777cb7f4b81431ee0f457880f52033ece
EvgenyRogoznev <rogoznevvvvv@gmail.com>
2019-10-12 00:30:13 +0500

    gradle

---
+++ src/main/java/TodoServis/Main.java
@@ -0,0 +1,8 @@
+package TodoServis;
+
+public class Main {
+    public static void main(String []args){
+        System.out.println("Write you code here" +
+                "Библиотеки подключать в файлк build.gradle");
+    }
+}
