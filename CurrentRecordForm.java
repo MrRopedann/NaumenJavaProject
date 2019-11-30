be8cd7f588e507acdb1bff63b0ae8e0821f1f220
vlady-san <56253272+vlady-san@users.noreply.github.com>
2019-11-30 22:53:23 +0500

    Add files via upload

---
+++ CurrentRecordForm.java
@@ -0,0 +1,20 @@
+package njp.NaumenJavaProject.forms;
+
+public class CurrentRecordForm {
+    private long id;
+
+    public CurrentRecordForm() {
+    }
+
+    public CurrentRecordForm(long id ) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ MainController.java
@@ -0,0 +1,61 @@
+package njp.NaumenJavaProject.controllers;
+
+import njp.NaumenJavaProject.forms.CurrentRecordForm;
+import njp.NaumenJavaProject.forms.RegistrationForm;
+import njp.NaumenJavaProject.models.Record;
+import njp.NaumenJavaProject.servises.RecordServices;
+import njp.NaumenJavaProject.servises.UsersServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    public String getCurrentUsername() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getName();
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+    public String/* ModelAndView*/ recordsForCurrentusers(Model model) {
+
+      //  RecordServices  recordServices = new RecordServices()  ;
+        UsersServices usersServices = new UsersServices();
+        List <Record> records = usersServices.findAll(getCurrentUsername(),false);
+      /*  Record recordCurentUser = recordServices.findByLogin(getCurrentUsername());
+        Record record=recordCurentUser;
+*/
+        //Set<Record>
+      // Record record= records.get(0);
+      //  ModelAndView mav =new ModelAndView();
+
+        model.addAttribute("records", records);
+        return("../static/index");
+       // return mav;
+
+
+    }
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ModelAndView regPost (@ModelAttribute CurrentRecordForm currentRecordForm) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/editing");
+        RecordServices recordServices =new RecordServices();
+        Record record = recordServices.findById(currentRecordForm.getId());
+        mav.addObject("note",record.getNote());
+        return mav;
+    }
+    }
+
+
